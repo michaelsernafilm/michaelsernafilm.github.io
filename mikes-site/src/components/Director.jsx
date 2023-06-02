4cbff682afbe0107c4599bbbf0bf4093add93241
mserna <matthewserna714@gmail.com>
2023-06-01 21:29:00 -0700

    ref correct scss

--- mikes-site/src/components/Director.jsx
+++ mikes-site/src/components/Director.jsx
@@ -4,9 +4,7 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 
-import "../css/video-react.css";
-import "../fonts/Analogue/Other Font Files/stylesheet.css";
-import '../../node_modules/react-modal-video/scss/modal-video.scss';
+import "./../css/model-video.scss"
 import ModalVideo from 'react-modal-video';
 
 import Card from '@mui/material/Card';
